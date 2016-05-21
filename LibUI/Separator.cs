2c27a7ce622b1254c80e7bb435e0954513c19c71
Calvin <calvin@openmailbox.org>
2016-05-21 20:48:57 -0300

    separator

---
+++ LibUI/Separator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibUI
+{
+    /// <summary>
+    /// A horizontal separator.
+    /// </summary>
+    public class HSeparator : Control
+    {
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern IntPtr uiSeparator();
+
+        public HSeparator()
+        {
+            Substrate = uiSeparator();
+        }
+    }
+}
