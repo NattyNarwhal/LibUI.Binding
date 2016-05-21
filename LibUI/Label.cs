8fda72543e3dde6c89d2b74f03c448bd6ba2d51c
Calvin <calvin@openmailbox.org>
2016-05-21 20:27:41 -0300

    label

---
+++ LibUI/Label.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibUI
+{
+    /// <summary>
+    /// A decorative text label.
+    /// </summary>
+    public class Label : Control
+    {
+        #region Interop
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern string uiLabelText(IntPtr control);
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern void uiLabelSetText(IntPtr control, string text);
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern IntPtr uiNewLabel(string text);
+        #endregion
+
+        /// <summary>
+        /// Creates a new button.
+        /// </summary>
+        /// <param name="text">The text on the button.</param>
+        public Label(string text)
+        {
+            Substrate = uiNewLabel(text);
+        }
+
+        /// <summary>
+        /// Gets of sets the label's text.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return uiLabelText(Substrate);
+            }
+            set
+            {
+                uiLabelSetText(Substrate, value);
+            }
+        }
+    }
+}
