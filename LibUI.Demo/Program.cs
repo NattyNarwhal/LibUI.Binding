214f1024d2d1bb7d8608057eabc63a7e012b9169
Calvin <calvin@openmailbox.org>
2016-05-21 18:13:51 -0300

    We no longer need this

--- LibUI.Demo/Program.cs
+++ LibUI.Demo/Program.cs
@@ -13,21 +13,15 @@ namespace LibUI.Demo
         static void Main(string[] args)
         {
             Application.Init();
+
             Window w = new Window("Hello world", 300, 300, false);
-
             Button b = new Button("I don't do anything");
-
             VBox x = new VBox();
-
-#if DEBUG
-            Console.WriteLine(w.Handle);
-            Console.WriteLine(b.Handle);
-            Console.WriteLine(x.Handle);
-#endif
 
             w.Child = x;
             x.Append(b);
             w.Visible = true;
+
             Application.Main();
             Application.Finish();
         }
