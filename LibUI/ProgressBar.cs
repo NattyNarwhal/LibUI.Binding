3d83356680877efffd355f6c99adf6cdb199b12e
Calvin <calvin@openmailbox.org>
2016-05-21 22:01:54 -0300

    progress bar

---
+++ LibUI/ProgressBar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibUI
+{
+    /// <summary>
+    /// A bar that fills as progress is achieved.
+    /// </summary>
+    public class ProgressBar : Control
+    {
+        #region Interop
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern void uiProgressBarSetValue(IntPtr control, long value);
+        [DllImport("libui.dll", CallingConvention = CallingConvention.Cdecl)]
+        protected static extern IntPtr uiProgressBar();
+        #endregion
+
+        /// <summary>
+        /// Creates a progress bar.
+        /// </summary>
+        public ProgressBar()
+        {
+            Substrate = uiProgressBar();
+        }
+
+        public long Value
+        {
+            set
+            {
+                uiProgressBarSetValue(Substrate, value);
+            }
+        }
+    }
+}
